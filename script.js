36cfcd3d1b0e5b719192c9cdb7b7c415030b875b
andrefelype <andrefilype21122014@gmail.com>
2026-05-06 22:52:26 -0300

    desenvolvendo o site

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-console.log("tudo ok");
 
-gsap.registerPlugin(DrawSVGPlugin, ScrollTrigger);
-
-//draws all elements with the "draw-me" class applied
-gsap.from(".draw-me", {
-  duration: 4,
-  drawSVG: 0,
-  scrollTrigger: {
-    trigger: ".logo-rosto",
-    markers: false,
-    start: "30% 50%",
-  },
-});
