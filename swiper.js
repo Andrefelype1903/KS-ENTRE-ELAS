a4b06bd49c092ab9b1bbbed0ff8f0eb163f42eb9
andrefelype <andrefilype21122014@gmail.com>
2026-05-08 22:50:46 -0300

    terminando slider

--- swiper.js
+++ swiper.js
@@ -37,7 +37,38 @@ const swiper2 = new Swiper(".swiper-2", {
   },
 
   pagination: {
-    el: ".swiper-pagination",
+    el: ".swiper-pagination-2",
+    clickable: true,
+  },
+
+  breakpoints: {
+    // when window width is >= 320px
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    // when window width is >= 640px
+    1188: {
+      slidesPerView: 5,
+      spaceBetween: 40,
+    },
+  },
+});
+
+const swiper3 = new Swiper(".swiper-3", {
+  loop: true,
+  navigation: {
+    nextEl: ".custom-next2",
+    prevEl: ".custom-prev2",
+  },
+
+  pagination: {
+    el: ".swiper-pagination-2",
     clickable: true,
   },
 
