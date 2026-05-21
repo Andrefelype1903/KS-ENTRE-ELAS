318c02199cfa6f771e3b4a3e8ad70bbae4e91aa0
andrefelype <andrefilype21122014@gmail.com>
2026-05-20 21:31:03 -0300

    avançando com GSAP

--- script.js
+++ script.js
@@ -185,13 +185,182 @@ gsap.from(".caixa-informacoes p", {
 
 // detaques ↑
 
+// merece ↓
+
 gsap.from(".merece .draw-me", {
   duration: 7,
   drawSVG: 0,
 
-  /* scrollTrigger: {
-    trigger: ".destaques",
+  scrollTrigger: {
+    trigger: ".cuidados-2",
+    markers: false,
+    start: "top 60%",
+  },
+});
+
+let h2Merece = SplitText.create(".merece h2", {
+  type: "lines, words, chars",
+  mask: "lines",
+});
+
+let h3Merece = SplitText.create(".merece h3", {
+  type: "lines, words, chars",
+  mask: "words",
+});
+
+gsap.from(h2Merece.chars, {
+  y: 100,
+  opacity: 0,
+  duration: 0.5,
+  stagger: 0.05,
+
+  scrollTrigger: {
+    trigger: ".merece h2",
     markers: false,
     start: "top 80%",
-  }, */
+    end: "bottom 70%",
+    scrub: 1,
+  },
+});
+
+gsap.from(h3Merece.chars, {
+  // x: 20,
+  opacity: 0,
+  duration: 0.5,
+  stagger: 0.05,
+
+  scrollTrigger: {
+    trigger: ".merece .cuidado",
+    markers: false,
+    start: "top 70%",
+  },
+});
+
+gsap.from(".merece p", {
+  y: 20,
+  opacity: 0,
+  duration: 0.5,
+  stagger: 0.5,
+
+  scrollTrigger: {
+    trigger: ".merece .cuidado",
+    markers: false,
+    start: "top 70%",
+  },
+});
+
+gsap.from(".merece img", {
+  // x: -20,
+  opacity: 0,
+  duration: 1,
+  stagger: 0.8,
+
+  scrollTrigger: {
+    trigger: ".merece .cuidado",
+    markers: false,
+    start: "top 70%",
+  },
+});
+
+// merece ↑
+
+// serviços ↓
+
+gsap.from(".titulo-secao4 h3", {
+  y: -20,
+  opacity: 0,
+  duration: 1,
+  scrollTrigger: {
+    trigger: ".sevicos",
+    markers: false,
+    start: "top 70%",
+  },
+});
+
+gsap.from(".titulo-secao4 h2", {
+  y: 20,
+  opacity: 0,
+  duration: 1,
+  scrollTrigger: {
+    trigger: ".sevicos",
+    markers: false,
+    start: "top 70%",
+  },
+});
+
+gsap.from(".sevicos .draw-me", {
+  duration: 4,
+  drawSVG: 0,
+
+  scrollTrigger: {
+    trigger: ".sevicos",
+    markers: false,
+    start: "top 70%",
+  },
+});
+
+let sevico1 = SplitText.create("#servico-1", {
+  type: "lines, words, chars",
+  mask: "lines",
+});
+
+let sevico2 = SplitText.create("#servico-2", {
+  type: "lines, words, chars",
+  mask: "lines",
+});
+
+let sevico3 = SplitText.create("#servico-3", {
+  type: "lines, words, chars",
+  mask: "lines",
+});
+
+gsap.from(sevico1.chars, {
+  opacity: 0,
+  stagger: 0.05,
+
+  scrollTrigger: {
+    trigger: "#servico-1",
+    markers: false,
+    start: "top 80%",
+    end: "bottom 60%",
+    scrub: 1,
+  },
+});
+
+gsap.from(sevico2.chars, {
+  opacity: 0,
+  stagger: 0.05,
+
+  scrollTrigger: {
+    trigger: "#servico-2",
+    markers: false,
+    start: "top 80%",
+    end: "bottom 60%",
+    scrub: 1,
+  },
+});
+
+gsap.from(sevico3.chars, {
+  opacity: 0,
+  stagger: 0.05,
+
+  scrollTrigger: {
+    trigger: "#servico-3",
+    markers: false,
+    start: "top 80%",
+    end: "bottom 60%",
+    scrub: 1,
+  },
+});
+
+gsap.from(".slider:nth-child(2) .destaque path", {
+  drawSVG: 0,
+  duration: 2,
+
+  scrollTrigger: {
+    trigger: ".slider:nth-child(2) .destaque path",
+    markers: false,
+    start: "top 80%",
+    end: "bottom 60%",
+  },
 });
